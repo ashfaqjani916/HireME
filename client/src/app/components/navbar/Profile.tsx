2891362905d1f3d1ba8a8716d045d3e5f3df33dc
Nikhil Pulluri <nikhilpulluri7810@gmail.com>
2024-10-28 21:31:21 +0530

    groups adding fix-partial correction requried

--- client/src/app/components/navbar/Profile.tsx
+++ client/src/app/components/navbar/Profile.tsx
@@ -3,6 +3,7 @@ import { useSession } from 'next-auth/react'
 import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
 import { Button } from '@/components/ui/button'
 import { HoverCard, HoverCardContent, HoverCardTrigger } from '@/components/ui/hover-card'
+import Image from 'next/image'
 
 interface User {
   name?: string | null
@@ -12,10 +13,6 @@ interface User {
 export default function Profile() {
   const { data: session } = useSession()
 
-  // if (session && session.user) {
-  //   console.log(session.user.image) // Logs the image URL or undefined if it doesn’t exist
-  // }
-
   if (session && session.user) {
     return (
       <HoverCard>
@@ -24,11 +21,7 @@ export default function Profile() {
         </HoverCardTrigger>
         <HoverCardContent className="w-80">
           <div className="flex justify-around items-center px-[30px] space-x-4">
-            <Avatar className="w-12 h-12">
-              {/* <AvatarImage src={`${session.user.image}?timestamp=${new Date().getTime()}`} alt="User Profile Image" /> */}
-              <img src={`${session.user.image}`} />
-              {/* <AvatarFallback>NP</AvatarFallback> */}
-            </Avatar>
+            {session.user.image !== null ? <Image className="h-8 w-8 rounded-full" alt={`${session.user.name}`} height={200} width={200} src={`${session.user.image}`} /> : <div>I</div>}
             <div className="space-y-1">
               <h4 className="text-sm font-semibold">{session.user.name}</h4>
             </div>
